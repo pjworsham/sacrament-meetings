b960fb7a384fedae252cd15a7119057f15f73db5
Pam Worsham <worsham.pj@gmail.com>
2026-07-14 16:34:28 -0400

    font and image

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/Christ.webp"
+          alt="christ"
           width={100}
           height={20}
           // style={{ width: "auto", height: "auto" }}
